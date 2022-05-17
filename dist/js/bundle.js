51893a2f3d6342a12b83da460c1e8be326e8f39d
Mihail Voevodin <mrfirst3333@gmail.com>
2022-05-17 21:02:35 +0300

    add film card

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -89,13 +89,18 @@ function scroll() {
 __webpack_require__.r(__webpack_exports__);
 function select() {
 
+
     const _apiKey = 'ba2becc0-f421-4ef5-bf44-ebac95a88660',
           apiMyFilm = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
 
     const selectItem = document.querySelector('.film'),
           listFilms = document.querySelector('.my-list__select'),
           inputClose = document.querySelector('.new-film__close'),
-          movies = document.querySelector('.my-list__items');
+          movies = document.querySelector('.my-list__items'),
+          cards = document.querySelector('.cards');
+          
+
+          
 
 
     function getRatingColor(rate) {
@@ -149,17 +154,19 @@ function select() {
             console.log(item);
             
             displayFilm += `
-            <li class='movie'>
+            <li class='movie' filmId=${item.filmId}>
                 <img src="${item.posterUrl}" alt="${item.nameEn}">
                 <div class="movie__name movie__nameEn">${item.nameEn ? item.nameEn : ''}</div>
                 <div class="movie__name movie__nameRu">${item.nameEn ? '( ' + item.nameRu + ' )' : item.nameRu}</div>
                 <div class="movie__genres">${item.genres.slice(0, 3).map(genre => ` ${genre.genre}`)}</div>
                 <div class="movie__rating movie__rating_${getRatingColor(item.rating)}">${item.rating != 'null' ? getRating(item.rating) : 'no'}</div>
                 <div class="movie__close">Удалить</div>
+                <div class="movie__info">Подробнее...</div>
             </li>
             `
             console.log(item.rating);
             movies.innerHTML = displayFilm;
+            
         })
         
     }
@@ -183,37 +190,61 @@ function select() {
         })
 
         const resData = await res.json();
+        
         resData.films.slice(0, 6).forEach(item => {
-            const film = document.createElement('li');
-            film.classList.add('film-item')
-            film.innerHTML = `
-                ${item.nameRu}  
-            `
-            listFilms.appendChild(film);
-            film.addEventListener('click', () => {
-                console.log(item);
-                const movie = document.createElement('li');
-                
-                movie.classList.add('movie')
-                movie.innerHTML = `
-                    <img src="${item.posterUrl}" alt="${item.nameEn}">
-                    <div class="movie__name movie__nameEn">${item.nameEn ? item.nameEn : ''}</div>
-                    <div class="movie__name movie__nameRu">${item.nameEn ? '( ' + item.nameRu + ' )' : item.nameRu}</div>
-                    <div class="movie__genres">${item.genres.slice(0, 3).map(genre => ` ${genre.genre}`)}</div>
-                    <div class="movie__rating movie__rating_${getRatingColor(item.rating)}">${item.rating != 'null' ? getRating(item.rating) : 'no'}</div>
-                    <div class="movie__close">Удалить</div>
-                `
-                movies.appendChild(movie);
-                listFilms.innerHTML = '';
-                selectItem.value = '';
-                closeInput()
-                
-                moviesStorage.push(item);
-                console.log(moviesStorage);
-                localStorage.setItem('movies', JSON.stringify(moviesStorage));
-                deleteFilm()
-             })
             
+                const film = document.createElement('li');
+                film.classList.add('film-item')
+            
+                film.innerHTML = `
+                    ${item.nameRu}  
+                `
+                listFilms.appendChild(film);
+
+                film.addEventListener('click', () => {
+
+                    const movie = document.createElement('li');
+
+                    movie.setAttribute('filmId', item.filmId);
+                    movie.classList.add('movie');
+
+
+                    const ids = document.querySelectorAll('li.movie');
+                    const filmId = ids.map(item => {
+                        const id = item.getAttribute('filmid');
+                        console.log(id)
+                        return id;
+                    })
+                    console.log(ids);
+                    console.log(filmId);
+                    console.log(item.filmId);
+                    // if (item.filmId != filmId) {
+                        movie.innerHTML = `
+                            <img src="${item.posterUrl}" alt="${item.nameEn}">
+                            <div class="movie__name movie__nameEn">${item.nameEn ? item.nameEn : ''}</div>
+                            <div class="movie__name movie__nameRu">${item.nameEn ? '( ' + item.nameRu + ' )' : item.nameRu}</div>
+                            <div class="movie__genres">${item.genres.slice(0, 3).map(genre => ` ${genre.genre}`)}</div>
+                            <div class="movie__rating movie__rating_${getRatingColor(item.rating)}">${item.rating != 'null' ? getRating(item.rating) : 'no'}</div>
+                            <div class="movie__close">Удалить</div>
+                            <div class="movie__info">Подробнее...</div>
+                        `
+                        movies.appendChild(movie);
+                        listFilms.innerHTML = '';
+                        selectItem.value = '';
+                        closeInput()
+                        
+                        moviesStorage.push(item);
+                        console.log(moviesStorage);
+                        localStorage.setItem('movies', JSON.stringify(moviesStorage));
+                        deleteFilm()
+                        createCard()
+                    // } else {
+                    //     alert('This film in yhe list yet!');
+                    // }
+                    
+                })
+            
+             
         })
 
         window.addEventListener('click', (e) => {
@@ -233,6 +264,76 @@ function select() {
     
     deleteFilm()
 
+    function closeCard() {
+        const card = document.querySelector('.card')
+        
+        cards.addEventListener('click', (e) => {
+            console.log(e.target);
+            if (!e.target.closest('.card')) {
+                
+                e.target.innerHTML = '';
+                cards.style.display = 'none';
+                document.body.style.overflow = '';
+            }
+            
+
+        })
+    }
+
+
+    function createCard() {
+
+        const cardTriggers = document.querySelectorAll('.movie__info');
+
+        cardTriggers.forEach(e => {
+            
+            const filmId = e.parentNode.getAttribute('filmid');
+                
+            e.addEventListener('click', () => {
+                console.log(filmId);
+                moviesStorage = JSON.parse(localStorage.getItem('movies'));
+                moviesStorage.forEach(item => {
+                    
+                    if (item.filmId == filmId) {
+                        console.log(item)
+                        const card = document.createElement('div');
+                        cards.style.display = 'block';
+                        card.classList.add('card')
+                        card.innerHTML = `
+                            <img class="card__img" src="${item.posterUrl}" alt="${item.nameEn}">
+                            <div class="card__info">
+                                <div class="card__name">${item.nameEn ? item.nameEn : ''} ${item.nameEn ? '(' + item.nameRu +')' : item.nameRu}</div>
+                                <div class="card__type">Type : ${item.type}</div>
+                                <div class="card__year">Year : ${item.year}</div>
+                                <div class="card__countries">Countries : ${item.countries.map(countries => ` ${countries.country}`)}</div>
+                                <div class="card__genres">Genres :${item.genres.map(genre => ` ${genre.genre}`)}</div>
+                                <div class="card__rating">Rating : ${item.rating}</div>
+                                <div class="card__length">Length : ${item.filmLength}</div>
+                                <div class="card__descr">Description :<br>${item.description}</div>
+                            </div>
+                            
+                        `
+                        cards.appendChild(card);
+                        document.body.style.overflow = 'hidden';
+                        console.log(card);
+                        closeCard()
+                    }
+                    
+                })
+                    
+            })
+        })
+
+    }
+
+    
+
+    createCard()
+    
+
+    
+    
+    
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (select);
